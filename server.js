a265b5765961b4c6b5b2e9650dc5e40dd6916343
Steven Andrews <osidesigns@gmail.com>
2020-10-15 00:17:07 -0500

    Removed unwanted files and install needed dependencies.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 // Declare "Static" folder
 app.use(express.static(path.join(__dirname, 'public')));
